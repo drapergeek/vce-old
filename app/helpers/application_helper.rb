680cafd2bab968c090abfce34c5167cd91670759
Draper <gdraper@rsitmcltw8249.local>
2010-01-27 09:09:46 -0500

    EHELP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,17 +3,14 @@ module ApplicationHelper
   
   
   def can_view_item(controller, action)
-<<<<<<< HEAD
     if logged_in?
         if current_user.roles.detect {|role| role.rights.detect {|right| right.action.downcase == action && right.controller.downcase == controller } } 
           return true
         else
           return false
         end
-=======
     if current_user.roles.detect {|role| role.rights.detect {|right| right.action.downcase == action && right.controller.downcase == controller } } 
       return true
->>>>>>> b77ba55... updated the authentication system to be authlogic WOOT WOOT
     else
       return false
     end
