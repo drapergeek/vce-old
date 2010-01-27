0ceb20992c903bae1631fa4027b5e28589827ffb
Draper <gdraper@rsitmcltw8249.local>
2010-01-27 09:19:09 -0500

    sldjfklsdf

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :rights
   map.resources :units
 
+    map.resources :users
+   map.resource  :session
+   map.signup '/signup', :controller => 'users', :action => 'new'
+   map.login  '/login', :controller => 'sessions', :action => 'new'
+   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   
   # Sample of regular route:
