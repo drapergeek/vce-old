dcb688cf8819a6aca22a840c7519e81dbd48f878
draper <draper@rsitmcltw8249.local>
2010-04-22 09:22:56 -0400

    added the config for e-mail, maybe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ config.action_controller.perform_caching             = true
 config.action_view.debug_rjs                         = true
 
 #This is for action mailer
-config.action_mailer.delivery_method = :smtp
+
 ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true,
   :address => 'smtp.gmail.com',
