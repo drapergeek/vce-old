a594c65c893ed38cd5800d0558a9fdb8c1fdaaed
Jason Draper <gdraper@vt.edu>
2011-01-07 14:42:05 -0500

    upgraded to rails 3, just the inital command tho

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,51 +1,26 @@
+Vce::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
-# Settings specified here will take precedence over those in config/environment.rb
+  # In the development environment your application's code is reloaded on
+  # every request.  This slows down response time but is perfect for development
+  # since you don't have to restart the webserver when you make code changes.
+  config.cache_classes = false
 
-# In the development environment your application's code is reloaded on
-# every request.  This slows down response time but is perfect for development
-# since you don't have to restart the webserver when you make code changes.
-config.cache_classes = false
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_view.debug_rjs             = true
+  config.action_controller.perform_caching = false
 
-# Enable the breakpoint server that script/breakpointer connects to
-#config.breakpoint_server = true
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
 
-config.action_view.debug_rjs                         = true
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+end
 
-# Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = true
-
-#config.action_mailer.delivery_method = :smtp
-#config.action_mailer.smtp_settings = {
-#:address => "auth.smtp.vt.edu",
-#:port => 465,
-#:authentication => :login,
-#:domain=>"vt.edu",
-#:user_name => "gdraper",
-#:password => "mjisseXee2me@vt",
-#:tls=>true
-#}
-#config.action_mailer.smtp_settings = {
-#    :enable_starttls_auto => true,
-#    :address => "smtp.gmail.com",
-#    :port => "587",
-#    :domain => "google.com",
-#    :authentication => :plain,
-#    :user_name => "drapersjunk",
-#    :password => "lx8143xpmac"
-#}
-ActionMailer::Base.smtp_settings = {
-  :enable_starttls_auto => true,
-  :address => 'smtp.gmail.com',
-  :port => 587,
-  :domain => 'gmail.com',
-  :authentication => :plain,
-  :user_name => '4hprogrammer',
-  :password => 'simple4hpassword'
-}
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vce::Application.config.secret_token = 'fbfdd4998686b14aa0e40dc9dd71e4ec4395583bff5cd08ad4b14f3a9a860252060bd6e4cdb623918aabd6944c1da44feea32d024cd611702f8cb3d78e57801a'
