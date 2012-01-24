d2dff6e9ed3de10b72f5e2b14a0d3f9afaef5acf
Jason Draper <jason@drapergeek.com>
2012-01-23 22:18:34 -0500

    fixed the receipt refunds to use jquery instead of rjs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-function change_payment_method(){
-	if (document.getElementById("receipt_payment_method_cash").checked) {
-		Element.hide("receipt_payment_extra_input");
-	}
-	else{
-			Element.show("receipt_payment_extra_input");
-	};
-	
-}
+$(document).ready(function() {
 
-
-function change_collage(){
-	if (document.getElementById("receipt_payment_method_cash").checked) {
-		Element.hide("receipt_payment_extra_input");
-	}
-	else{
-			Element.show("receipt_payment_extra_input");
-	};
-	
-}
+  $("#refund_link").click(function() {
+    $("#refund_boxes").toggle();  
+    return false;
+  });
+  
+});
