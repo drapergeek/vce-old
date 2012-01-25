21b125013c306c4d4b8a7922749320e3e7043498
Jason Draper <jason@drapergeek.com>
2012-01-24 21:31:28 -0500

    added the javascript to watch the campers inputs and update the payment, now we need to work with the collage button

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
   /*Receipts*/
 
-   //hide or show the refund options
+  //hide or show the refund options
   $("#refund_link").click(function() {
     $("#refund_boxes").toggle();  
     return false;
@@ -18,10 +18,53 @@ $(document).ready(function() {
     else{
       $("#receipt_payment_extra_input").slideDown();
     };
-    return false;
+  return false;
   });
 
 
-  /* End Receipts */
+
+  //Watch the camper spots for change so we can update everything
+  $(".camper_name_input").change(function(){
+      //Set up all our names
+      id = $(this).attr("id");
+      camper_number = id.substring(id.length-1);
+      collage = "#receipt_camper" + camper_number + "_collage";
+      camper_id = "#receipt_camper" + camper_number + "_id";
+      payment = "#receipt_camper" + camper_number + "_payment";
+    if (!$(this).val()) {
+      //we need to clear the value in the amount box and remove the collage checkbox
+      $(collage).removeAttr("checked");
+      $(camper_id).val("");
+      $(payment).val("");
+    }
+    else{
+      //We need to calculate the camp price and add it to the box at the end
+      camp_price = $("#camp_price").val();
+      $(payment).val(camp_price);
+    };
+    updateFinalPrice();
+  return false;
+  });
+
   
+  /* End Receipts */
+
 });
+
+function updateFinalPrice() {
+  camper1 = numFromString("#receipt_camper1_payment");
+  camper2 = numFromString("#receipt_camper2_payment");
+  camper3 = numFromString("#receipt_camper3_payment");
+  total_amount = camper1 + camper2 + camper3 
+  $("#receipt_amount").val(total_amount);
+}
+
+function numFromString(string){
+  if ($(string).val()) {
+     return parseFloat($(string).val()); 
+  }
+  else{
+     return 0;
+  };
+}
+
