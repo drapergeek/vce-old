134639c27cce09acb5df1ac790d379fef8f43196
Jason Draper <jason@drapergeek.com>
2012-01-29 11:56:54 -0500

    fixed up the payments information a bit

--- app/helpers/campers_helper.rb
+++ app/helpers/campers_helper.rb
@@ -15,5 +15,21 @@ module CampersHelper
       input
     end
   end
+
+  def dob(camper)
+    if camper.dob.nil?
+     "Birthdate: Not Set" 
+    else
+     "Birthdate: #{@camper.dob.strftime("%B %d, %Y")}" 
+    end
+  end
+
+  def tetnus_shot(camper)
+    if camper.last_tetnus_shot.blank?
+      "Date of last tetnus shot: Not Set" 
+    else
+      "Date of last tetnus shot: #{@camper.last_tetnus_shot.strftime(' %B %d, %Y ')}"
+    end
+  end
   
 end
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,2 @@
+Time::DATE_FORMATS[:default] = "%m/%e/%y %I:%M%p"
+Date::DATE_FORMATS[:default] = "%m/%e/%y"
