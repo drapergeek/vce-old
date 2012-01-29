ef1db4a276fc10082e9a53d43cfd7a2e25d8b2f1
Jason Draper <jason@drapergeek.com>
2012-01-29 12:51:11 -0500

    removed a few more int to string changes

--- db/migrate/012_add_position_to_camper.rb
+++ db/migrate/012_add_position_to_camper.rb
@@ -1,6 +1,6 @@
 class AddPositionToCamper < ActiveRecord::Migration
   def self.up
-    add_column :campers, :position, :integer
+    add_column :campers, :position, :string
   end
 
   def self.down
