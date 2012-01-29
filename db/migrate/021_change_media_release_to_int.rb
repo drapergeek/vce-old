f93b9bf515be16961b727245c3b213bf08b74033
Jason Draper <jason@drapergeek.com>
2012-01-29 12:48:13 -0500

    didnt delete the file

--- db/migrate/021_change_media_release_to_int.rb
+++
@@ -1,9 +0,0 @@
-class ChangeMediaReleaseToInt < ActiveRecord::Migration
-  def self.up
-    change_column :campers, :media_release, :integer
-  end
-
-  def self.down
-    change_column :campers, :media_release, :boolean
-  end
-end
