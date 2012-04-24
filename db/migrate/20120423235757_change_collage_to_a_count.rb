d389f93706db0cbeab056780f6684885743044ea
Jason Draper <jason@drapergeek.com>
2012-04-23 21:25:32 -0400

    change to removing the column and adding it

--- db/migrate/20120423235757_change_collage_to_a_count.rb
+++ db/migrate/20120423235757_change_collage_to_a_count.rb
@@ -2,9 +2,15 @@ class ChangeCollageToACount < ActiveRecord::Migration
   def up
     add_column :campers, :collage_count, :integer, :default => 0
     remove_column :campers, :collage_purchased
-    change_column :receipts, :camper1_collage, :integer, :default => 0
-    change_column :receipts, :camper2_collage, :integer, :default => 0
-    change_column :receipts, :camper3_collage, :integer, :default => 0
+
+    remove_column :receipts, :camper1_collage, :string
+    add_column :receipts, :camper1_collage, :integer, :default => 0
+
+    remove_column :receipts, :camper2_collage, :string
+    add_column :receipts, :camper2_collage, :integer, :default => 0
+
+    remove_column :receipts, :camper3_collage, :string
+    add_column :receipts, :camper3_collage, :integer, :default => 0
   end
 
   def down
