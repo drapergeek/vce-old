ad4917741c04ba4ed57bd19f483d511a1cd5cb25
Jason Draper <jason@drapergeek.com>
2012-04-28 10:42:41 -0400

    use a hash to remove duplication

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -37,7 +37,7 @@ class Receipt < ActiveRecord::Base
     Receipt.all.each do |receipt|
       Receipt.all.each do |r|
         if r.id != receipt.id && r.fname == receipt.fname && r.lname == receipt.lname
-          dups[r.id] << r
+          dups[recipt.id] << receipt
         end
       end
     end
